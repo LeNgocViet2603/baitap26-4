79e355af6180da7543cc2de3b48fb06de478bd87
= <=>
2022-05-10 07:45:57 +0700

    tim max min

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using Internal;
 using System;
 public class diemdanh
 {
-   
     public static void Main(string[] args)
     {
         int n=10;
@@ -13,6 +12,18 @@ public class diemdanh
             Console.WriteLine("Nhap so thu {0}",i+1);
             a[i]=Console.ReadLine();
         }
-
+        int max_168=a[0];
+        int min_168=a[0];
+        for (var j = 0; j < n; j++)
+        {
+            if(a[i]>max_168){
+                max=a[i];
+            }                
+            if(a[i]<min_168){
+                min_168 = a[i];
+            }
+        }
+    
+        
     }
 }
